e7af0bdde86abc9e432f5ce32b4b559ad3fcfba1
Nemhis <p.nemphis@gmail.com>
2019-08-20 00:09:12 +0300

    Create task data stub

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const createTask = () => ({
+  description: [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`][Math.floor(Math.random() * 3)],
+  dueDate: Date.now() + Math.pow(-1, Math.floor(Math.random() * 3)) + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000, // +- неделя
+  repeatingDays: {
+    'mo': false,
+    'tu': Boolean(Math.round(Math.random())),
+    'we': false,
+    'th': Boolean(Math.round(Math.random())),
+    'fr': false,
+    'sa': Boolean(Math.round(Math.random())),
+    'su': false,
+  },
+  tags: new Set(shuffleArray([
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`,
+    `relax`,
+  ]).slice(0, Math.round(Math.random() * 3))),
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  isFavorite: Boolean(Math.round(Math.random())),
+  isArchive: Boolean(Math.round(Math.random())),
+});
+
+
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
